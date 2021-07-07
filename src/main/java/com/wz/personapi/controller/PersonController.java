d2b92a260ec1c8c86229fd30d2d6031a73900416
Welington <welington_zanon@hotmail.com>
2021-07-07 09:50:19 -0300

    delet, update

--- src/main/java/com/wz/personapi/controller/PersonController.java
+++ src/main/java/com/wz/personapi/controller/PersonController.java
@@ -1,7 +1,16 @@
 package com.wz.personapi.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.wz.personapi.DTO.PersonDTO;
 import com.wz.personapi.services.PersonService;
 
+import javassist.NotFoundException;
 import lombok.AllArgsConstructor;
 
 @RestController
@@ -23,4 +33,26 @@ public class PersonController {
 		return service.insert(dto);
 	}
 	
+	@GetMapping
+	public ResponseEntity<Page<PersonDTO>> findAll(Pageable pageable){
+		PageRequest pageRequest = PageRequest.of(pageable.getPageNumber(), pageable.getPageSize(), Sort.by("firstName"));
+		Page<PersonDTO> list = service.findAllPaged(pageRequest);
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<PersonDTO> findById(@PathVariable Long id){
+		PersonDTO dto = service.findByID(id);
+		return ResponseEntity.ok().body(dto);
+	}
+	
+	@DeleteMapping("/{id}")
+	public void deleteById(@PathVariable Long id) throws NotFoundException {
+		service.delete(id);
+	}
+	
+	@PutMapping("/{id}")
+	public PersonDTO updateById(@PathVariable Long id, @RequestBody PersonDTO dto) {
+		return service.updateById(id, dto);
+	}
 }
--- src/main/java/com/wz/personapi/services/PersonService.java
+++ src/main/java/com/wz/personapi/services/PersonService.java
@@ -1,13 +1,20 @@
 package com.wz.personapi.services;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.config.ConfigDataResourceNotFoundException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.wz.personapi.DTO.PersonDTO;
 import com.wz.personapi.entities.Person;
 import com.wz.personapi.mapper.PersonMapper;
 import com.wz.personapi.repositories.PersonRepository;
 
+import javassist.NotFoundException;
 import lombok.AllArgsConstructor;
 
 @Service
@@ -18,10 +25,35 @@ public class PersonService {
 	
 	private final PersonMapper personMapper = PersonMapper.INSTANCE;
 	
+	@Transactional
 	public PersonDTO insert(PersonDTO dto) {
 		Person personToSave = personMapper.toModel(dto);
 		Person savePerson = personRepository.save(personToSave);
 		return new PersonDTO(savePerson);
 	}
 	
+	@Transactional(readOnly = true)
+	public Page<PersonDTO> findAllPaged(Pageable pageable){
+		Page<Person> page = personRepository.findAll(pageable);
+		return page.map(x -> new PersonDTO(x));
+	}
+	
+	@Transactional(readOnly = true)
+	public PersonDTO findByID(Long id) {
+		Optional<Person> obj = personRepository.findById(id);
+		Person entity = obj.orElseThrow(() -> new ConfigDataResourceNotFoundException(null));
+		return new PersonDTO(entity);
+	}
+	
+	@Transactional
+	public void delete (Long id) throws NotFoundException{
+		personRepository.deleteById(id);
+	}
+	
+	@Transactional
+	public PersonDTO updateById(Long id, PersonDTO personDTO) {
+		Person personToUpdate = personMapper.toModel(personDTO);
+		Person updatedPerson = personRepository.save(personToUpdate);
+		return new PersonDTO(updatedPerson);
+	}
 }
